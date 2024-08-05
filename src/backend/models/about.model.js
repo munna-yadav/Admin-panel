4f8cb251c6b4f03e091d4b6971782b982771a435
munna-yadav <mail.munna50@gmail.com>
2024-08-05 13:54:29 +0530

    feat: Add aobut Us model

--- src/backend/models/about.model.js
+++ src/backend/models/about.model.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 const aboutUsSchema = new mongoose.Schema(
   {
-    title:{
-        type: String,
-        required: true
-    },
+    
     description:{
         type: String,
         required: true,
